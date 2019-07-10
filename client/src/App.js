8ed030c1867f7255d65de9e0618205b5f34ad514
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-10 11:02:42 -0400

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          THIS IS THE BOMBS
         </a>
       </header>
     </div>
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
-
+const path = require('path');
 const express = require('express');
 
 const server = express();
 
 server.use(express.json());
+server.use(express.static(path.join(__dirname, 'client/build')));
 
 server.get('/', (req, res) => {
-  console.log('meh');
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 server.listen(process.env.PORT, () => {
